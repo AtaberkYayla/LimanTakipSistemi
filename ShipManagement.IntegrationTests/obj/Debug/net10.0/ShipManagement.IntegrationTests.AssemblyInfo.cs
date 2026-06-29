76d73b88073528af0d902ed0cd01e582d299e49d
Ataberk YAYLA <bimar.stajyer1@arkas.com>
2026-06-29 09:28:03 +0300

    Add FluentValidation validators for all entities

--- ShipManagement.IntegrationTests/obj/Debug/net10.0/ShipManagement.IntegrationTests.AssemblyInfo.cs
+++ ShipManagement.IntegrationTests/obj/Debug/net10.0/ShipManagement.IntegrationTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShipManagement.IntegrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8be1498ebdee88859a2b6d1f0daea882d4cf456")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShipManagement.IntegrationTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShipManagement.IntegrationTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ShipManagement.Tests/obj/Debug/net10.0/ShipManagement.Tests.AssemblyInfo.cs
+++ ShipManagement.Tests/obj/Debug/net10.0/ShipManagement.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ShipManagement.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8be1498ebdee88859a2b6d1f0daea882d4cf456")]
 [assembly: System.Reflection.AssemblyProductAttribute("ShipManagement.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ShipManagement.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
